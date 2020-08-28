00261afe224c99e460a387ec8a29418e81694aea
Rubén Cabrera <dev@rubencabrera.es>
2020-08-28 18:21:55 +0100

    feat: ejecución rápida y hasta luego

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ MAINTAINER Rubén Cabrera Martínez <dev@rubencabrera.es>
 
 RUN apk update && apk add perl
 RUN apk add perl-libwww curl
+COPY dinaip.conf /etc/dinaip.conf
 ADD source /app
 WORKDIR /app
 RUN ["sh", "./install.sh"]
--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -1,6 +1,27 @@
 #!/bin/ash
 
 set -e
+if [[ -z "${USUARIO}" ]]; then
+  echo "USUARIO no definido"
+  exit 1
+elif [[ -z "${PASSWORD}" ]]; then
+  echo "PASSWORD no definida"
+  exit 1
+fi
 
-# Si las env var no están asignadas, se piden las credenciales por stdin y fail
-dinaip -u $USUARIO -p $PASSWORD
+alias dina='dinaip -u "${USUARIO}" -p "${PASSWORD}"'
+# dinaip -u "${USUARIO}" -p "${PASSWORD}" -a rubencabrera.es:www
+# echo "-i"
+# dina -i
+# echo "Añadiendo zona www"
+# dina
+echo "-l Lista los dominios de esta cuenta"
+dina -l
+sleep 5
+echo "-s Muestra el estado"
+dina -s
+# Para meter otra zona este es el comando.
+# TODO: ejecutar opcionalmente si le pasamos los argumentos necesarios
+# echo "Vamos a meter otra zona NUEVA"
+# dina -a rubencabrera.art:prueba
+echo "Fin del script, hasta luego"
