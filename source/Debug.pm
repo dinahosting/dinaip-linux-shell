c5efcc38b9d8ff45ddf7c4b32e04e40fa0677613
gitnatxo <isilla@belate.net>
2023-01-19 12:54:15 +0100

    Update Debug.pm

--- source/Debug.pm
+++ source/Debug.pm
@@ -6,25 +6,34 @@ use POSIX qw(strftime);
 my $INICIADO = undef;
 my $DEBUG = undef;
 
+my $PRIMERPLANO = undef;
+
 sub log{
 
-	&iniciar unless($INICIADO);
-	
-	my $fecha = strftime ("%Y-%m-%d %H:%M:%S",localtime());
-	print $DEBUG "[$fecha] $_[0] \n";	
+        &iniciar unless($INICIADO);
+
+        my $fecha = strftime ("%Y-%m-%d %H:%M:%S",localtime());
+        print $DEBUG "[$fecha] $_[0] \n";
 
 }
 
 sub iniciar{
+        if ($PRIMERPLANO == 1) {
+                open ($DEBUG, '>>', '/dev/stdin') ||
+                        die($!);
+        } else {
+                open ($DEBUG, '>>', '/var/log/dinaip.log') ||
+                        die($!);
+        }
 
-	open ($DEBUG, '>>', '/var/log/dinaip.log') ||
-		die($!);
+        $DEBUG->autoflush;
 
-	$DEBUG->autoflush;
+        $INICIADO = 1;
+}
 
-	$INICIADO = 1;
+sub setPrimerPlano{
+        $PRIMERPLANO = $_[0];
 }
 
 
 1;
-
